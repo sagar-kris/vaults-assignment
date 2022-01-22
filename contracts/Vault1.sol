3bd979a5eca6ccf90eb7aa01c0702e77d50697fa
Sagar Krishnaraj <sagar.krishnaraj@workday.com>
2022-01-21 20:48:56 -0600

    test structure

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+import "hardhat/console.sol";
+
 contract Vault1 {
     
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit(uint8 _amount) payable external {
+        
+    }
+
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -4,10 +4,24 @@ const { ethers } = require("hardhat");
 describe("Vault 1", () => {
 	beforeEach(async () => {
 		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		[account] = await ethers.getSigners();
 
 		vault1 = await Vault1.deploy();
 
 		await vault1.deployed();
 	});
 });
+
+describe("deposit", function () {
+	it("should revert when amount is zero", async function () {
+		await expect(
+			vault1.connect().deposit(1000)
+		).to.be.revertedWith("Amount is zero");	
+	});
+
+	it("should revert when called on an unsupported ERC-20 token", async function () {
+		await expect(
+			vault1.connect().deposit()
+		).to.be.revertedWith("Unsupported ERC-20 token");	
+	});
+})
